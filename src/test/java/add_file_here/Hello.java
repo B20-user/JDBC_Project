bf1761caadee2ff86f17446cb406542487047abe
tiger1911 <mehmetatas@yahoo.com>
2020-10-07 21:45:37 +0200

    Added some lines to describe what we are doing

--- src/test/java/add_file_here/Hello.java
+++ src/test/java/add_file_here/Hello.java
@@ -3,5 +3,8 @@ package add_file_here;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
+
+        //we have added one new branch called us123
+        // to work on user story 123 without affecting master
     }
 }
