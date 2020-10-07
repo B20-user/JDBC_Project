f8de2d2075560226ad73640ee23cd7f6c63eea02
Abdurrahman <a.akay26@gmail.com>
2020-10-07 17:31:56 +0100

    Commit !!!

---
+++ src/test/java/add_file_here/EU3_Practice.java
@@ -0,0 +1,12 @@
+package add_file_here;
+
+public class EU3_Practice {
+
+    public static void main(String[] args) {
+
+        System.out.println("EU3 opening pull request");
+
+        //new changes!
+
+    }
+}
