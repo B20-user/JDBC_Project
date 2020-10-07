ed2ea5bbe137be75cf3b483b60fe387a6211e768
Erhan <erhanilgar@gmail.com>
2020-10-07 17:23:42 +0100

    new print added

---
+++ src/test/java/add_file_here/TestClass.java
@@ -0,0 +1,8 @@
+package add_file_here;
+
+public class TestClass {
+    public static void main(String[] args) {
+        System.out.println("Akbar You are Awesome");
+        System.out.println("Thank you for your all effort");
+    }
+}
