4da7cd8007a19223e65c67a82b361fd92a04eb39
gulgulyil <gulnurgulnurxxf@gmail.com>
2020-10-07 22:32:56 +0200

    adding more hero

--- src/test/java/add_file_here/AvengerInfinity.java
+++ src/test/java/add_file_here/AvengerInfinity.java
@@ -3,5 +3,13 @@ package add_file_here;
 public class AvengerInfinity {
     public static void main(String[] args) {
         //add four changes and commit with 4 seperate commit message
+
+        //adding more info about Avengers
+
+        //Iron man, Thor, Hulk
+
+        //Looks like we don't have much avenger fan?
+
+        //Justice league fan maybe?
     }
 }
