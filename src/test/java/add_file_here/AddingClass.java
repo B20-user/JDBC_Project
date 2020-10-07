d1cee62e60a294a05678ffe21a754a133d53a7a3
CundullahT <cundullahterzioglu.it@gmail.com>
2020-10-07 17:37:09 +0200

    I added some nice files.

---
+++ src/test/java/add_file_here/AddingClass.java
@@ -0,0 +1,11 @@
+package add_file_here;
+
+public class AddingClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hi, I'm adding some class if you don't mind.");
+
+    }
+
+}
