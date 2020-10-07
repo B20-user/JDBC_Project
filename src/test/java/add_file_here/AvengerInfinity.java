2eff6af3656c1d501e72998ae56dc8785c0293a1
Musajan Obul <musajanobul@gmail.com>
2020-10-07 22:30:30 +0200

    one output

---
+++ src/test/java/add_file_here/AvengerInfinity.java
@@ -0,0 +1,13 @@
+package add_file_here;
+
+
+
+public class AvengerInfinity {
+    public static void main(String[] args) {
+        System.out.println("Rsotte Nebula is the newest nenbula I know");
+        System.out.println("Asesome Avenger fans here");
+
+
+    }
+
+}
