e91ac5894709f91af07580c58b45b0ee40bd492b
CundullahT <cundullahterzioglu.it@gmail.com>
2020-10-07 22:31:33 +0200

    Third Change for AvengerInfinity Class, and typo from first change is fixed.

--- src/test/java/add_file_here/AvengerInfinity.java
+++ src/test/java/add_file_here/AvengerInfinity.java
@@ -4,9 +4,11 @@ public class AvengerInfinity {
 
     public static void main(String[] args) {
 
-        //  add four changes and commit with 4 separate commit messages.    --->       First Change.
+        //  Add four changes and commit with 4 separate commit messages.    --->       First Change.
 
         System.out.println("Second Change");
+
+        //  Adding third one
 
     }
 
