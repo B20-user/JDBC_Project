48b2e622e5a96becf6e265c01b2fd896f429033a
Sami <samikale90@gmail.com>
2020-10-07 20:45:32 +0100

    added some owesome lines to describe what we are doing in us123 branch

--- src/test/java/add_file_here/EU3_Practice.java
+++ src/test/java/add_file_here/EU3_Practice.java
@@ -3,5 +3,8 @@ package add_file_here;
 public class EU3_Practice {
     public static void main(String[] args) {
         System.out.println("EU is opening a pull request");
+
+        //we have added one new branch called us123
+        //to work on user story 123 without affecting master
     }
 }
