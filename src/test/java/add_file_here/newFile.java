4876f0b29c814b8b35f91e8fee0e5fce8b0ba862
Mehmet Sanal <mehmetsanal2020@gmail.com>
2020-10-07 21:45:30 +0200

    added some new line for using branch us123

--- src/test/java/add_file_here/newFile.java
+++ src/test/java/add_file_here/newFile.java
@@ -5,5 +5,9 @@ public class newFile {
     public static void main(String[] args) {
 
         System.out.println("Helloooooo");
+
+        //we have added one new branch called us123
+
+        //to work on user story 123 without affecting master
     }
 }
