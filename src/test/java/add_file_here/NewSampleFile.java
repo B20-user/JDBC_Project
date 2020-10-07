e08a2d311756efd79551be33406c7ec1b3f4fe68
Dennis <ankaraizmir312@gmail.com>
2020-10-07 20:45:34 +0100

    added some awesome line to describe what we are doing on us123

--- src/test/java/add_file_here/NewSampleFile.java
+++ src/test/java/add_file_here/NewSampleFile.java
@@ -3,5 +3,8 @@ package add_file_here;
 public class NewSampleFile {
     public static void main(String[] args) {
         System.out.println("Much more awesome stuff here!!");
+        //we have added one new branch called us123
+        //to work on user story 123 without affecting master
+        //master - default branch!!
     }
 }
