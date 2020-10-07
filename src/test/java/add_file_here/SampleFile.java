8eadd10b83588fb130ff909248d808e098c039c2
Musajan Obul <musajanobul@gmail.com>
2020-10-07 21:46:55 +0200

    Some output added

--- src/test/java/add_file_here/SampleFile.java
+++ src/test/java/add_file_here/SampleFile.java
@@ -6,6 +6,9 @@ public class SampleFile {
 
         System.out.println("Awesome stuff here");
 
+        System.out.println("I am practicing IntelliJ ");
+        System.out.println("I am practicing Git github ");
+
     }
 
 }
