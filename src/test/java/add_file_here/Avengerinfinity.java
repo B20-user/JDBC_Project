ae4cdca94272897c306515fcc59b965c71ed105b
tiger1911 <mehmetatas@yahoo.com>
2020-10-07 22:31:26 +0200

    Added new file

---
+++ src/test/java/add_file_here/Avengerinfinity.java
@@ -0,0 +1,14 @@
+package add_file_here;
+
+public class Avengerinfinity {
+
+    public static void main(String[] args) {
+
+        // add four changes and commit with 4 separate commit messages
+
+        int n=1;
+        if(n<10){
+            System.out.println("Try again");
+        }
+    }
+}
