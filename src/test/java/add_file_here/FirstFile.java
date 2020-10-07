71f61a13b98db70910f3cb7f6c0316714a2a8ae8
IsmetYigitbasi <ismetyigitbasi@gmail.com>
2020-10-07 18:25:22 +0200

    Added First File by Ismet

---
+++ src/test/java/add_file_here/FirstFile.java
@@ -0,0 +1,12 @@
+package add_file_here;
+
+public class FirstFile {
+
+    public static void main(String[] args) {
+
+
+        for (int i=0;i<5;i++){
+            System.out.println("This is the first class. Ready to go...");
+        }
+    }
+}
