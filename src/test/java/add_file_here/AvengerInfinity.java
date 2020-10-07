e680b3de28e05a0b460a7a8ad64b68831fc898e0
Carola Marchani <carola.marchani@gmail.com>
2020-10-07 22:29:41 +0200

    add new functionalitites

--- src/test/java/add_file_here/AvengerInfinity.java
+++ src/test/java/add_file_here/AvengerInfinity.java
@@ -5,5 +5,7 @@ public class AvengerInfinity {
     public static void main(String[] args) {
 
         System.out.println("My favorite food is ceviche");
+
+        System.out.println("Spain is lovely");
     }
 }
