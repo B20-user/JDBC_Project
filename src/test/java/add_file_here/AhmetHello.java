8aa205a35c1aff4a9d33e71f4d277bea603bd94a
Ahmet Tasci <ahmet@lincsafe.com>
2020-10-07 18:45:43 +0200

    Second change=2

--- src/test/java/add_file_here/AhmetHello.java
+++ src/test/java/add_file_here/AhmetHello.java
@@ -3,7 +3,5 @@ package add_file_here;
 public class AhmetHello {
 
     public static void main(String[] args) {
-        System.out.println("Hello Akbar!");
-        System.out.println("New change-2-");
-    }
+        System.out.println("Hello Akbar!");}
 }
