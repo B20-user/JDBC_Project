f36030c4a5ce78b3d6d1259abf0028f8070cddad
MehmetAkifTUNC <kilyneu@gmail.com>
2020-10-07 18:03:35 +0200

    added one class

---
+++ src/test/java/add_file_here/Kily.java
@@ -0,0 +1,12 @@
+package add_file_here;
+
+import org.junit.Test;
+
+public class Kily {
+
+    public static void main(String[] args) {
+        System.out.println("TestNg @ Annotations");
+        System.out.println("@Test , @....");
+
+    }
+}
