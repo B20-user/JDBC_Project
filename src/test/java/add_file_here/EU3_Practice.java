a97f9ffc94d19888395e6ea9bfdb9fd92ace084b
Zeynep Ozkan <zeynepozkan0320@gmail.com>
2020-10-07 19:21:06 +0200

    second request

--- src/test/java/add_file_here/EU3_Practice.java
+++ src/test/java/add_file_here/EU3_Practice.java
@@ -4,16 +4,8 @@ public class EU3_Practice {
 
     public static void main(String[] args) {
 
-        System.out.println("EU3 is opening pull request");
+        System.out.println("I am not sure that I have done correctly");
 
-        //commit your change
-        //with nice commit message
-        //push it back to the github
-        //observe the change
-        //from the your github repo
-            //click on compare
-        //create a pull request (PR)
-            //to asl original repo owner
-            //to merge your change
+
     }
 }
