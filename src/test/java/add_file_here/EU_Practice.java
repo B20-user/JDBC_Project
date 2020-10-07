12b94470ce29615d3d3c96ff7ea4d09c7ba7c112
SerdarCalikoglu <serdarcalikoglu54@gmail.com>
2020-10-07 20:45:58 +0100

    Added some new changes anew branches

--- src/test/java/add_file_here/EU_Practice.java
+++ src/test/java/add_file_here/EU_Practice.java
@@ -3,7 +3,13 @@ package add_file_here;
 public class EU_Practice {
 
     public static void main(String[] args) {
+
         System.out.println("Hello a new beautiful day:)))");
+
+        // These are all new tools
+        // We have added one new branch called us123
+
+
     }
 
 
