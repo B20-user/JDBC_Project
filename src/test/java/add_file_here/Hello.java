06295c09c148214f11aec42563e55ec817372329
aytek <aytekbilici@gmail.com>
2020-10-07 18:26:38 +0200

    second commit

--- src/test/java/add_file_here/Hello.java
+++ src/test/java/add_file_here/Hello.java
@@ -5,5 +5,7 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
+        //commit then push it with a message
+        //observe the change
     }
 }
