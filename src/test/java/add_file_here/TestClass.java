12f81bf449cb6a959241ecaa888131abd88f7893
Erhan <erhanilgar@gmail.com>
2020-10-07 17:46:14 +0100

    new print added

--- src/test/java/add_file_here/TestClass.java
+++ src/test/java/add_file_here/TestClass.java
@@ -4,5 +4,7 @@ public class TestClass {
     public static void main(String[] args) {
         System.out.println("Akbar You are Awesome");
         System.out.println("Thank you for your all effort");
+        //check the compare button
+        System.out.println("Hello");
     }
 }
