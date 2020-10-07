655d71d00e115f221dd5914de088715421428511
AhmetFlz <vdt.7186@gmail.com>
2020-10-07 22:43:32 +0300

    Ahmet branch userstory123 changing 1

--- src/test/java/add_file_here/addingFile.java
+++ src/test/java/add_file_here/addingFile.java
@@ -4,5 +4,7 @@ public class addingFile {
     public static void main(String[] args) {
         System.out.println("this is first adding after cloning");
         System.out.println("I created that class");
+        // this is Userstory123 branch stuff
+
     }
 }
