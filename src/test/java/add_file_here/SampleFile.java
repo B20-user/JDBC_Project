185c0140c96d2ed989ab7bbce67c43fa200ada0f
Erhan <erhanilgar@gmail.com>
2020-10-07 16:54:50 +0100

    new print added

--- src/test/java/add_file_here/SampleFile.java
+++ src/test/java/add_file_here/SampleFile.java
@@ -5,6 +5,8 @@ public class SampleFile {
     public static void main(String[] args) {
 
         System.out.println("Awesome stuff here");
+        System.out.println("Akbar You are Awesome");
+
 
     }
 
