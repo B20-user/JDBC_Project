58da40e448412387bdfa9f2aac5c1f41a8cb1390
Rida <ridatat@gmail.com>
2020-10-07 23:30:50 +0300

    About cities added

--- src/test/java/add_file_here/avengerInitiy.java
+++ src/test/java/add_file_here/avengerInitiy.java
@@ -4,5 +4,6 @@ public class avengerInitiy {
     public static void main(String[] args) {
         //add four changes and commit with 4 seprate commit messagges
         //adding more info about fans club
+        //New york!
     }
 }
