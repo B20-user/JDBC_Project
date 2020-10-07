ed46bf6fdbd46b781f8e726fbc325bc282c7028b
Isha <65711600+Iysha1@users.noreply.github.com>
2020-10-07 17:41:20 +0100

    Add files via upload

---
+++ src/test/java/add_file_here/Question3.java
@@ -0,0 +1,19 @@
+package day_2;
+
+public class Question3 {
+
+	public static void main(String[] args) {
+		System.out.println(" Today weather is so nice");
+		System.out.println(" Today weather is so nice");
+		System.out.println(" Today weather is so nice");
+		System.out.println(" Today weather is so nice ");
+		System.out.println(" Today weather is so nice");
+		System.out.println(" Today weather is so nice");
+		System.out.println(" Today weather is so nice");
+		System.out.println(" Today weather is so nice");
+		System.out.println(" Today weather is so nice");
+		System.out.println(" Today weather is so nice");
+		
+	}
+
+}
