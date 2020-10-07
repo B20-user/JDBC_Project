34a0d472608ae90d04e63196629b448eea95d6cb
Ali Celik <aliihsancelik1@gmail.com>
2020-10-07 20:45:26 +0100

    Awesome Changes added awesomely

--- src/test/java/add_file_here/AwesomeFile.java
+++ src/test/java/add_file_here/AwesomeFile.java
@@ -7,5 +7,7 @@ public class AwesomeFile {
     @Test
     public void AwesomeTest(){
         System.out.println("Awesome");
+        System.out.println("this is awesome");
+        System.out.println("this is awesome");
     }
 }
