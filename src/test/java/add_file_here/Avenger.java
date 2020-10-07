df2a19123d1fc41e8d26b4594fc1bc8598dab8c6
aytek <aytekbilici@gmail.com>
2020-10-07 22:32:11 +0200

    eighth commit

--- src/test/java/add_file_here/Avenger.java
+++ src/test/java/add_file_here/Avenger.java
@@ -3,4 +3,6 @@ package add_file_here;
 public class Avenger {
 
     //add four changes and commit with 4 separate commit messages.
+    //something which I like to do is coding
+    //keep practice to keep up with class
 }
