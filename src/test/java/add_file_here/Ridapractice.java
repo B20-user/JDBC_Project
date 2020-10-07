e7e6e9fc8dc50b66b062c60b65b5e5d5a99ded7d
Rida <ridatat@gmail.com>
2020-10-07 22:45:33 +0300

    Added some text to describe what we are doing in JDBC-EU3

--- src/test/java/add_file_here/Ridapractice.java
+++ src/test/java/add_file_here/Ridapractice.java
@@ -4,5 +4,11 @@ public class Ridapractice {
 
     public static void main(String[] args) {
         System.out.println("Eu3 is opening pull request");
+
+
+
+        // we have added one new branch called JDBC_eu3
+        // to work on user story without affecting master
+        //hello man!
     }
 }
