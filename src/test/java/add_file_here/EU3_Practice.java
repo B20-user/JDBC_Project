28b7e73e10d484d678eb366d20a130e8f8ac27c7
pinar3146 <pinaryanar3146@hotmail.com>
2020-10-07 18:45:36 +0200

    added a new  repo

---
+++ src/test/java/add_file_here/EU3_Practice.java
@@ -0,0 +1,21 @@
+package add_file_here;
+
+public class EU3_Practice {
+    public static void main(String[] args) {
+        System.out.println("EU3 is opening pull a new class");
+      //commit your change
+        //with nice commit message
+        //push it back
+        //observe the change
+        //from your gtihub repo
+        //click on compare
+        // create a pull request
+        //to aslk original repo owner
+        //to merge your change
+
+
+
+
+    }
+
+}
