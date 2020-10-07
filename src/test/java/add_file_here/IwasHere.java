e2c2289a25b3f50d135b2abcd8247c057d17f86e
yusuftml <yusuftemel355@gmail.com>
2020-10-07 21:45:35 +0200

    add new line for branch(userstory123)

--- src/test/java/add_file_here/IwasHere.java
+++ src/test/java/add_file_here/IwasHere.java
@@ -4,5 +4,8 @@ public class IwasHere {
     public static void main(String[] args) {
 
         System.out.println("Pull or Push that is Question");
+
+        // i  added new branch for practice
+        // it called userstory123
     }
 }
