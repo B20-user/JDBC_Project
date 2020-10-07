cdcce64c81a20c518f3386e153b79e51d758adde
SarperCaglayan <sarpercaglayan16@gmail.com>
2020-10-07 19:35:08 +0300

    This is the change

---
+++ src/test/java/add_file_here/EU_Practice.java
@@ -0,0 +1,23 @@
+package add_file_here;
+
+public class EU_Practice {
+    public static void main(String[] args) {
+
+        System.out.println("EU3 is opening pull reqouest");
+
+        //commit your change
+        // with some nice commit message
+        //push it back to github
+        //observe the change
+        //from the your GitHub repo
+            // click on compare
+        //create a pull request (PR)
+            // to asl original repo owner
+            // to merge your change
+
+
+
+
+
+    }
+}
