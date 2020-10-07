514a855aa29348154a1ff582655b519daf385aa2
Ilyas Ulgur <ulgur@yahoo.com>
2020-10-07 22:29:35 +0200

    second change on nebula...

--- src/test/java/add_file_here/AvengerInfinity.java
+++ src/test/java/add_file_here/AvengerInfinity.java
@@ -3,5 +3,7 @@ package add_file_here;
 public class AvengerInfinity {
     public static void main(String[] args) {
         //add four changes and commit with 4 separate commit messages
+
+        //I still struggle to do my exercise on the fault in my first steps...
     }
 }
