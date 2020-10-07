c20ac6b5071735c8fc303816ce514d16a981659f
kdrsyl <kadersayligb@gmail.com>
2020-10-07 17:25:16 +0100

    new file added

---
+++ src/test/java/add_file_here/Git_Practise.java
@@ -0,0 +1,11 @@
+package add_file_here;
+
+public class Git_Practise {
+    public static void main(String[] args) {
+
+
+        System.out.println("new file is added");
+    }
+
+    //new file is here
+}
