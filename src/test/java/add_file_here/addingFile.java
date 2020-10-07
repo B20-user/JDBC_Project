f2a48542b33f4825bbc9873e410be87528e84d88
AhmetFlz <vdt.7186@gmail.com>
2020-10-07 19:24:54 +0300

    Ahmet commit 1

---
+++ src/test/java/add_file_here/addingFile.java
@@ -0,0 +1,8 @@
+package add_file_here;
+
+public class addingFile {
+    public static void main(String[] args) {
+        System.out.println("this is first adding after cloning");
+        System.out.println("I created that class");
+    }
+}
