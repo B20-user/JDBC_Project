4d7ba762a98912ebe94df8e254dc7c3ad174bbff
Nese-yigit <nese1988.ny@gmail.com>
2020-10-12 15:58:43 +0200

    deleted some line

--- src/test/java/add_file_here/AkbarClassIsGreat.java
+++ src/test/java/add_file_here/AkbarClassIsGreat.java
@@ -7,7 +7,7 @@ public class AkbarClassIsGreat {
     //to work on user story 123 without affecting master
 
     //adding more changes here so I can have more history
-    //more changes
-    //fixed typo form chnages to changes
+
+
 
 }
