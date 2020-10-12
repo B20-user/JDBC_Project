f6e025c7fd2c415414fc58648e8576b0ba4b254b
Enis Olgac <enisolgac26@gmail.com>
2020-10-12 10:35:12 +0300

    practice1 branch initial commit

--- src/test/java/add_fie_here/Hello.java
+++ src/test/java/add_fie_here/Hello.java
@@ -3,5 +3,6 @@ package add_fie_here;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hEEYYY test 001");
+        System.out.println("practice1 branch changes has been applied");
     }
 }
