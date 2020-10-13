336eed3ec98641b6903703bad970fbee62a4e067
Ercan Cotenoglu <ercancotenoglu@gmail.com>
2020-10-13 17:47:39 +0100

    feature commit2 22

--- src/test/java/feature2.java
+++ src/test/java/feature2.java
@@ -2,5 +2,9 @@ public class feature2 {
     public static void main(String[] args) {
 
         System.out.println("working on feature2 ");
+
+        System.out.println("working on feature2 ");
+        System.out.println("working on feature2 ");
+
     }
 }
