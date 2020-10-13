1afd1862c5e1828972755aeca7c8121e11a16e32
gaye <gungorgaye@gmail.com>
2020-10-13 15:20:00 +0300

    I add a new class here

---
+++ src/test/java/add_fie_here/EU3_Practice.java
@@ -0,0 +1,15 @@
+package add_fie_here;
+
+public class EU3_Practice {
+    public static void main(String[] args) {
+        System.out.println("EU3 is ready to pull request");
+        //commit your change
+        //push with nice commit  message
+        //push it back to the github
+        //observe the change from your own github repo
+        //click on compare
+        //create a pull request
+        //to ask originl repo owner to merge your change
+
+    }
+}
