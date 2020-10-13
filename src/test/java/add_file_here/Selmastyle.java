609e5465e33444fd6c1be405051e7ef467adedd0
Selma-Atabas <selma-aykal43@hotmail.com>
2020-10-14 00:49:13 +0200

    some changes

---
+++ src/test/java/add_file_here/Selmastyle.java
@@ -0,0 +1,8 @@
+package add_file_here;
+
+public class Selmastyle {
+
+    public static void main(String[] args) {
+        System.out.println("I cloned my project and I added some changes and now I will push it ");
+    }
+}
