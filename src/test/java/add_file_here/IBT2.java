05172a8cc347e69dcc84fda3bb47ec6078810abd
Ishak Ture <ishakture@gmail.com>
2020-10-13 11:51:20 +0100

    Change 1

---
+++ src/test/java/add_file_here/IBT2.java
@@ -0,0 +1,14 @@
+package add_file_here;
+
+public class IBT2 {
+
+    public static void main(String[]args){
+
+        // add four changes and commit with 4 separate commit msg.
+
+        System.out.println("Change 1");
+
+
+
+    }
+}
