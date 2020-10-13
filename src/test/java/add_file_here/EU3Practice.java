f5f83fc3182015184ce2279002bdcefb814abd6c
MustafaCelik-E <mustafacelik.eng@gmail.com>
2020-10-14 02:25:48 +0300

    added some extra lines on us123

--- src/test/java/add_file_here/EU3Practice.java
+++ src/test/java/add_file_here/EU3Practice.java
@@ -11,5 +11,7 @@ public class EU3Practice {
 
         // we have added one new branch called us123
         //to work on user story 123 without affecting master
+
+        // adding more changes so I can have more history
     }
 }
