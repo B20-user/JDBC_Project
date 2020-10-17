f709fe41e249ad9152f4d59c4d3fc1ac8cf7af43
mustafadurak <hhaattiicceecelik@gmail.com>
2020-10-17 11:59:27 +0200

    Added HelloAgain hi class

--- src/test/java/add_file_here/HelloAgain.java
+++ src/test/java/add_file_here/HelloAgain.java
@@ -1,7 +1,10 @@
 package add_file_here;
 
+import java.sql.SQLOutput;
+
 public class HelloAgain {
     public static void main(String[] args) {
         System.out.println("Hello again");
+        System.out.println("hi");
     }
 }
