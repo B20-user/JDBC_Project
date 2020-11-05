8197aab942307fd10d7d7daa59927fd461c58f97
sonerkucuk <sonerkucuk@msn.com>
2020-11-05 12:54:13 +0100

    yeni sayfa new page pull request

--- src/test/java/add_fille_here/EU3_Practice.java
+++ src/test/java/add_fille_here/EU3_Practice.java
@@ -3,6 +3,11 @@ package add_fille_here;
 public class EU3_Practice {
     public static void main(String[] args) {
         System.out.println("Eu3 is opining  pull request");
+        System.out.println("Eu3 is opining  pull request");
+        System.out.println("Eu3 is opining  pull request");
+        System.out.println("Eu3 is opining  pull request");
+        System.out.println("Eu3 is opining  pull request");
+
     }
 
 }
