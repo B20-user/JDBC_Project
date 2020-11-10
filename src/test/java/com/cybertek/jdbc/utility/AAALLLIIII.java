21ea8664febe130c34ee7e71587904845d8887cf
myDell <alikonya@gmail.com>
2020-11-10 21:39:20 +0000

    more more commit

--- src/test/java/com/cybertek/jdbc/utility/AAALLLIIII.java
+++ src/test/java/com/cybertek/jdbc/utility/AAALLLIIII.java
@@ -3,4 +3,9 @@ package com.cybertek.jdbc.utility;
 public class AAALLLIIII {
 
     //i wrote an awesome code for that functionalty
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
 }
